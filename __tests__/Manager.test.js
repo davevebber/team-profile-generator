d2afb3830226b354349000b0fea8df4cb9ac4972
dave vebber <davevebber@gmail.com>
2021-03-25 13:17:40 -0700

    all test craeted and passed

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
+test("Can set office number", () => {
+    const testValue = 100;
+    const e = new Manager("Dave", 1, "test@test.com",
+    testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+
+test("Should return 'Manager'", () => {
+    const testValue = "Manager";
+    const e = new Manager("Dave", 1, "test@test.com", 100);
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number", () => {
+    const testValue = 100;
+    const e = new Manager("Dave", 1, "test@test.com",
+    testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
